952c50b8ecb7f9013c4c22d96661346a233f1acb
xin <xin@questlabs.ai>
2024-12-18 14:07:56 +0800

    fix bug of color theme

--- nextjs/middlewares/colorTheme.ts
+++ nextjs/middlewares/colorTheme.ts
@@ -4,12 +4,13 @@ import appConfig from 'configs/app';
 import * as cookiesLib from 'lib/cookies';
 
 export default function colorThemeMiddleware(req: NextRequest, res: NextResponse) {
-  const colorModeCookie = req.cookies.get(cookiesLib.NAMES.COLOR_MODE);
+  // const colorModeCookie = req.cookies.get(cookiesLib.NAMES.COLOR_MODE);
 
-  if (!colorModeCookie) {
-    if (appConfig.UI.colorTheme.default) {
-      res.cookies.set(cookiesLib.NAMES.COLOR_MODE, appConfig.UI.colorTheme.default.colorMode, { path: '/' });
-      res.cookies.set(cookiesLib.NAMES.COLOR_MODE_HEX, appConfig.UI.colorTheme.default.hex, { path: '/' });
-    }
+  // if (!colorModeCookie) {
+  if (appConfig.UI.colorTheme.default) {
+    res.cookies.set(cookiesLib.NAMES.COLOR_MODE, appConfig.UI.colorTheme.default.colorMode, { path: '/' });
+    res.cookies.set(cookiesLib.NAMES.COLOR_MODE_HEX, appConfig.UI.colorTheme.default.hex, { path: '/' });
   }
+  //   }
+  // }
 }
